2b3689a073e8366a0272c25d06dd546eed8fd509
Georgie <george.okwori@outlook.com>
2024-05-04 15:17:53 +0100

    Update __init__.py

--- accountsphere/__init__.py
+++ accountsphere/__init__.py
@@ -13,7 +13,15 @@ if os.path.exists("env.py"):
 # Initialize Flask application
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY") # Set the secret key
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL") # Set the database URL
+
+if os.environ.get("DEVELOPMENT") == "True":
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL") # Set the database URL
+else:
+    uri = os.environ.get("DATABASE_URL")
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri
+
 
 # Initialize database management
 db = SQLAlchemy(app)
