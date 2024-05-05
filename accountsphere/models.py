6a26bae4224e9554a8c658e88a5f736262db7ae3
George Okwori <132067023+GOkwori@users.noreply.github.com>
2024-05-05 13:49:34 +0100

    Review and update py code inline with pep8

--- accountsphere/models.py
+++ accountsphere/models.py
@@ -1,10 +1,11 @@
+# Description: This file contains the database models for the application.
 from accountsphere import db
 from datetime import datetime
 from sqlalchemy.orm import relationship
 from flask_login import UserMixin
 
 
-class User(db.Model, UserMixin):
+class User(db.Model, UserMixin):    # Define the User model
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(100), nullable=False)
@@ -14,47 +15,51 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(120), unique=True, nullable=False)
     role = db.Column(db.String(50), nullable=False)
     active = db.Column(db.Boolean, default=True)  # Default as active
-    group_id = db.Column(db.Integer, db.ForeignKey('groups.id', ondelete='CASCADE'), nullable=True)
+    group_id = db.Column(db.Integer, db.ForeignKey(
+        'groups.id', ondelete='CASCADE'), nullable=True)
     created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
-    updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+    updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(
+    ), onupdate=db.func.current_timestamp())
 
-    def get_id(self):
-        return str(self.id)
+    def get_id(self):    # Get the user ID
+        return str(self.id)    # Return the user ID as a string
 
     @property
-    def is_active(self):
-        return self.active
+    def is_active(self):    # Check if the user is active
+        return self.active    # Return the active status of the user
 
 
-
-class Group(db.Model):
+class Group(db.Model):    # Define the Group model
     __tablename__ = 'groups'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=True, nullable=False)
     description = db.Column(db.Text)
     group_type = db.Column(db.String(50), nullable=False)
-    users = db.relationship('User', backref='group', cascade='all, delete', lazy=True)
+    users = db.relationship('User', backref='group',
+                            cascade='all, delete', lazy=True)
     created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
-    updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+    updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(
+    ), onupdate=db.func.current_timestamp())
 
-    def __repr__(self):
-        return f'<Group {self.name}>'
-    
+    def __repr__(self):    # Return the group name
+        return f'<Group {self.name}>'    # Return the group name
 
-class Product(db.Model):
+
+class Product(db.Model):    # Define the Product model
     __tablename__ = 'products'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     description = db.Column(db.Text, nullable=True)
     type = db.Column(db.String(50), nullable=False)
     # Define relationship to Account
-    accounts = db.relationship('Account', backref='product', cascade='all, delete', lazy=True)
+    accounts = db.relationship(
+        'Account', backref='product', cascade='all, delete', lazy=True)
 
-    def __repr__(self):
-        return f'<Product {self.name}>'
-    
+    def __repr__(self):    # Return the product name
+        return f'<Product {self.name}>'    # Return the product name
 
-class Account(db.Model):
+
+class Account(db.Model):    # Define the Account model
     __tablename__ = 'accounts'
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(100), nullable=False)
@@ -62,22 +67,24 @@ class Account(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     phone_number = db.Column(db.String(20), unique=True, nullable=True)
     date_of_birth = db.Column(db.Date, nullable=True)
-    product_id = db.Column(db.Integer, db.ForeignKey('products.id', ondelete='CASCADE'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey(
+        'products.id', ondelete='CASCADE'), nullable=False)
     account_type = db.Column(db.String(50), nullable=False)
     balance = db.Column(db.Numeric(10, 2), default=0.00)
     currency = db.Column(db.String(3), nullable=False, default='USD')
     status = db.Column(db.String(20), default='active')
     created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
-    updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+    updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(
+    ), onupdate=db.func.current_timestamp())
 
-    def __repr__(self):
-        return f'<Account {self.first_name} {self.last_name} | Account ID: {self.id}>'
-    
+    def __repr__(self):    # Return the account name
+        return f'<Account {self.first_name} {self.last_name} | Account ID: {self.id}>'    # Return the account name
 
-class NewsItem(db.Model):
+
+class NewsItem(db.Model):    # Define the NewsItem model
     id = db.Column(db.Integer, primary_key=True)
     headline = db.Column(db.String(255), nullable=False)
     description = db.Column(db.Text, nullable=False)
 
-    def __repr__(self):
-        return f'<NewsItem {self.headline}>'
+    def __repr__(self):    # Return the news item headline
+        return f'<NewsItem {self.headline}>'    # Return the news item headline
